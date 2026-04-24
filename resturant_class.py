f1083be41969abbf516047c7a1a19f2eda7661a4
Muayad2024 <muayadtrade@gmail.com>
2026-04-24 21:11:09 +0800

    Add project files.

---
+++ resturant_class.py
@@ -0,0 +1,22 @@
+
+class Resturant:
+    def __init__(self, name, cuisine_type):
+        self.name = name
+        self.cuisine_type = cuisine_type
+    def describe_restaurant(self):
+        print(f'The resturant name is {self.name} and it has {self.cuisine_type} cuisine.')
+    def open_restaurant(self):
+        print(f'{self.name} is now open!')
+
+resturant = Resturant('Pizza Hut', 'Italian Pizza')
+print(f'The resturant name is {resturant.name}')
+print(f'The resturant cuisine type is {resturant.cuisine_type}')
+resturant.describe_restaurant()
+resturant.open_restaurant()
+my_resturant = Resturant('KFC', 'Fried Chicken')
+your_resturant = Resturant('McDonalds', 'Burgers')
+our_resturant = Resturant('Subway', 'Sandwiches')
+my_resturant.describe_restaurant()
+your_resturant.describe_restaurant()
+our_resturant.describe_restaurant()
+
