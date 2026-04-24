247ce9e068572af536b767383c135a65926d77e3
Muayad2024 <muayadtrade@gmail.com>
2026-04-25 01:34:38 +0800

    Here I added a new son Class that is under the Resturant class, and I added new method to this new class

--- resturant_class.py
+++ resturant_class.py
@@ -12,10 +12,17 @@ class Resturant:
         self.number_served = new_number
     def increment_number_served(self, incr_number):
         self.number_served += incr_number
-
+class IceCreamStand(Resturant):
+    def __init__(self, name, cuisine_type):
+        super().__init__(name, cuisine_type)
+        self.flavors = ['Strawbery', 'Vanilla', 'Mango']
+    def display_flavores(self):
+        print(f'The avilable flavores: {self.flavors}')
+my_ice_cream = IceCreamStand('Mango Ice cream', 'Ice Cream')
+my_ice_cream.display_flavores()
 resturant = Resturant('Pizza Hut', 'Italian Pizza')
 """print(f'The resturant name is {resturant.name}')
-print(f'The resturant cuisine type is {resturant.cuisine_type}')"""
+print(f'The resturant cuisine type is {resturant.cuisine_type}')
 
 resturant.describe_restaurant()
 resturant.open_restaurant()
@@ -26,7 +33,7 @@ resturant.set_number_served(10)
 print(f'The customers the resturant served is {resturant.number_served}')
 resturant.increment_number_served(9)
 print(f'The customers the resturant served is {resturant.number_served}')
-"""my_resturant = Resturant('KFC', 'Fried Chicken')
+my_resturant = Resturant('KFC', 'Fried Chicken')
 your_resturant = Resturant('McDonalds', 'Burgers')
 our_resturant = Resturant('Subway', 'Sandwiches')
 my_resturant.describe_restaurant()
